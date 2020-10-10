c8ee643d855963eaeac9ab9e9273eef80da927ad
Iyogwoya Daniel Oshioke <oshioke@Iyogwoyas-MacBook-Pro.local>
2020-10-10 12:55:38 +0100

    worked on layout of the viewShipment page. responsivity and all

--- src/components/sec/ViewShipments/ViewShipments.jsx
+++ src/components/sec/ViewShipments/ViewShipments.jsx
@@ -2,25 +2,49 @@ import React, { useState } from 'react';
 import CartonCard from './components/CartonCard/CartonCard';
 import "./ViewShipments.scss";
 
-const array = [0,1,2,3,4,5,6,7];
+const array = [0,11,22,343,43,52,63,27,28,39,49,59,99,44,96,114];
 
 const ViewShipments = () =>{
-    const [activeCard, setActiveCard] = useState(0);
+    const [activeCardIdx, setActiveCardIdx] = useState(0);
+    const activeCarton = array.find((a, i)=> i === activeCardIdx);
+
+    const fetchCards = () =>{
+        // In here, all cards for the current month is fetched on mount;
+    }
+
     const onCardClick = (i) =>{
-        setActiveCard(i)
+        setActiveCardIdx(i);
+        // All shipment for
     }
     return(
         <div className = "view-shipments">
+            <div className = "header-section">
+
+            </div>
             <div className = "main-section">
                 <div className = "cartons-section">
+                    <div className = "title-section">
+                        <h5>Cartons in september</h5>
+                    </div>
                     { 
                         array.map((a, i) => (
-                            <CartonCard onClick = {()=>onCardClick(i)} active = {i === activeCard? true : false}/>
+                            <CartonCard
+                                onClick = {()=>onCardClick(i)} 
+                                active = {i === activeCardIdx? true : false}
+                                number = {a}
+                                shipments = {a+2}
+                                key = {i}
+                                weight = {a}
+                            />
                         ))
                     }
                 </div>
                 <div className = "shipments-section">
-
+                    <div className = "title-section">
+                        <h5>Shipments in carton {activeCarton}</h5>
+                    </div>
+                    <div className = "shipment-table">
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/sec/ViewShipments/components/CartonCard/CartonCard.jsx
+++ src/components/sec/ViewShipments/components/CartonCard/CartonCard.jsx
@@ -5,19 +5,19 @@ const CartonCard = props =>{
     return(
         <div onClick = {props.onClick} className = {`carton-card ${props.active ? 'active' : null} `}>
             <div className = "carton-number-section">
-                <h3 className = "carton-number-title">Carton Number</h3>
+                <h4 className = "carton-number-title">Carton <div>Number</div></h4>
                 <div className = 'number-container'>
-                    <h3>33</h3>
+                    <h4>{props.number}</h4>
                 </div>
             </div>
             <div className = 'details-section'>
                 <div className = "card-shipments-details">
-                    <p>Shipments</p>
-                    <p>33</p>
+                    <p className = "detail-title">Shipments</p>
+                    <p>{props.shipments}</p>
                 </div>
                 <div>
-                    <p>Weight</p>
-                    <p>33kg</p>
+                    <p className = "detail-title">Weight</p>
+                    <p>{props.weight}kg</p>
                 </div>
             </div>
         </div>
